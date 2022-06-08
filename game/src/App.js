5af3dda6944e35bfb0d09ac27959faf2248997d2
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-06-07 17:42:49 -0700

    Updated front screen

--- game/src/App.js
+++ game/src/App.js
@@ -7,7 +7,10 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <h1 className="Title">JeoPriydy</h1>
+    <>
+      <h1 className="Title">JeoPriydy</h1>
+      <h3>Welcome to JeoPriydy, the classic quiz game! To begin, choose the number of people and start the game!</h3>
+    </>
   )
 }
 
@@ -30,14 +33,33 @@ const Foot = () => {
 
 function App() {
   const [showMain, setshowMain] = React.useState(false)
+  let [players, setPlays] = React.useState(0)
   const showMainClick = () => {
     setshowMain(true)
+  }
+  function increaseTeams () {
+    if (players < 8) {
+      setPlays(players + 1)
+    }
+    else {setPlays(8)}
+  }
+  function decreaseTeams () {
+    if ( players === 0 ) {
+      players = 0
+    }
+    else {setPlays(players - 1)}
   }
   return (
     <div className="App">
       {!showMain && <Head />}
+      {!showMain && <div className="App">
+        <button className="playbutton" onClick={decreaseTeams}>-</button>
+        <div className="App players"> {players}
+        </div>
+        <button className="playbutton" onClick={increaseTeams}>+</button>
+      </div>}
       {!showMain && <button className="StartButton" onClick={showMainClick}>Start the game!</button>}
-      {showMain && <Main />}
+      {showMain && <div><Main /></div>}
       {showMain && <Foot />}
     </div>
   );
