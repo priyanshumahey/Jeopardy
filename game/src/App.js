5857ed6241cb42f418aff9f97e99ae9b5c93df08
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-06-10 09:40:43 -0700

    Update App.js
    Added Head Component

--- game/src/App.js
+++ game/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './App.css'
 
+function Head() {
+    return (      
+    <>
+        <h1 className="Title" >JeoPriydy</h1>
+        <h3>Welcome to JeoPriydy, the classic quiz game! To begin, choose the number of people and start the game!</h3>
+    </>
+    )
+};
+
+
 function App() {
     return (
-        <></>
+        <div className="App">
+            <Head />
+        </div>
     )
-}
+};
 
 
 export default App;
